b1e7c7a24d97819e51dc4229445ddb976598e088
jcserv <jarrod.servilla@gmail.com>
2025-02-03 17:12:50 -0500

    v1.1.4

--- Formula/nvy.rb
+++ Formula/nvy.rb
@@ -1,9 +1,9 @@
 class Nvy < Formula
   desc "A simple command line tool for managing multiple env files (profiles) in a project."
   homepage "https://github.com/jcserv/nvy"
-  url "https://github.com/jcserv/nvy/releases/latest/download/nvy-1.1.3-aarch64-apple-darwin.tar.gz"
-  sha256 "c42679ee05c943a6d9783c0b794470c350e4d48b704f84b582075a894eeb6f7a"
-  version "1.1.3"
+  url "https://github.com/jcserv/nvy/releases/latest/download/nvy-1.1.4-aarch64-apple-darwin.tar.gz"
+  sha256 "a66ea3a0306fb933f5d9fd1476089cd19221134033bda37b2de65b9bc6667b32"
+  version "1.1.4"
 
   def install
     bin.install "nvy"

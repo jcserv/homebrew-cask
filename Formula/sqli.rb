151aae9e4b5ffadf7974d0dca53ddd9c8d8e8941
jcserv <jarrod.servilla@gmail.com>
2025-03-01 22:55:07 -0500

    [sqli] v1.0.3

--- Formula/sqli.rb
+++ Formula/sqli.rb
@@ -1,9 +1,9 @@
 class Sqli < Formula
-  desc "sqli is a sleek SQL client, used as terminal-ui or command-line interface, that helps you query your databases"
+  desc "sqli is a simple & sleek SQL client, used as a terminal UI or in the command line, to help you query your Postgres databases"
   homepage "https://github.com/jcserv/sqli"
-  url "https://github.com/jcserv/sqli/releases/latest/download/sqli-1.0.2-aarch64-apple-darwin.tar.gz"
-  sha256 "ede725fb2f696f2d6f312c93120cb48b6dd43d74db1e90565aed0297d077849a"
-  version "1.0.2"
+  url "https://github.com/jcserv/sqli/releases/latest/download/sqli-1.0.3-aarch64-apple-darwin.tar.gz"
+  sha256 "0f0798beb14375db25b186347520d44fc97c28f4e3e88786855763fc8cadc466"
+  version "1.0.3"
 
   def install
     bin.install "sqli"

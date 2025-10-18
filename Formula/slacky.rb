6245117dd59c0dfd7881bd46b324e93176e7c0ee
jcserv <jarrod.servilla@gmail.com>
2025-10-18 14:34:00 -0400

    [slacky] v0.5.1

--- Formula/slacky.rb
+++ Formula/slacky.rb
@@ -1,9 +1,9 @@
 class Slacky < Formula
   desc "A simple, no-frills Slack client in the terminal"
   homepage "https://github.com/jcserv/slacky"
-  url "https://github.com/jcserv/slacky/releases/latest/download/slacky-0.5.0-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/jcserv/slacky/releases/latest/download/slacky-0.5.1-aarch64-apple-darwin.tar.gz"
   sha256 "831b2e9311a5af77f70a682044a28226b9010bfe95690d25757dcef0a9dea750"
-  version "0.5.0"
+  version "0.5.1"
 
   def install
     bin.install "slacky"
